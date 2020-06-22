23f3753cb249cbb48a4c0afd6fee73237c04a9ee
rofi <rofi260@gmail.com>
2020-06-22 09:37:16 +0700

    absensi

--- app/Http/Controllers/SatpamController.php
+++ app/Http/Controllers/SatpamController.php
@@ -1,13 +1,17 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\Crypt;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\company;
 use App\Client;
 use App\satpam;
 use App\profilesatpam;
+use App\User;
+use App\attendance;
+use App\schedule;
 
 class SatpamController extends Controller
 {
@@ -20,10 +24,31 @@ class SatpamController extends Controller
 
 
     //requst data satpam
-    public function index(){
-        $satpam = DB::table('satpam')->get();
+    public function index($role){
+        $data = Crypt::decrypt($role);
+        $nitip=User::findOrFail($data);
 
-        return view('/Satpam/ListofSatpam',['satpam' => $satpam]);
+        if ($nitip->role =='admin') {
+           
+            $satpam = satpam::all();
+
+            return view('/Satpam/ListofSatpam',['satpam' => $satpam]);
+           
+        }
+        elseif($nitip->role  == 'outsourcing'){
+            $satpam = satpam::where('outsourcing_id' , $nitip->company_id)
+            ->get();
+           
+            return view('/Satpam/ListofSatpam',['satpam' => $satpam]);
+
+        }elseif($nitip->role == 'client'){
+            
+            $satpam = satpam::where('client_id' , $nitip->client_id)
+            ->get();
+            return view('/Satpam/ListofSatpam',['satpam' => $satpam]);
+        }
+        
+       
     }
 
     public function account(){
@@ -32,33 +57,166 @@ class SatpamController extends Controller
         return view('/Satpam/AccountSatpam',['satpam' => $satpam]);
     }
 
-    public function deploy(){
-        // if (auth()->user()->role == 'admin') {
+    public function deploy($role){
+        $data = Crypt::decrypt($role);
+        $nitip=User::findOrFail($data);
+
+        if ($nitip->role =='admin') {
            
-        //     $client = Client::all();
-        //     $profil = profile_satpam::all();
+            $satpam = satpam::all();
 
-        //     return
-
-        // }
-        // elseif(auth()->user()->role == 'outsourcing'){
-        //     $satpam = satpam::where('company_id' , Auth::user()->id)
-        //     ->get();
+            return view('/Satpam/DeploymentSatpam',['satpam' => $satpam] );
            
+        }
+        elseif($nitip->role  == 'outsourcing'){
+            $satpam = satpam::where('outsourcing_id' , $nitip->company_id)
+            ->get();
+           
+            return view('/Satpam/DeploymentSatpam',['satpam' => $satpam] );
 
-        // }elseif(auth()->user()->role == 'client'){
+        }elseif($nitip->role == 'client'){
             
-        //     $satpam = satpam::where('client_id' , Auth::user()->id)
-        //     ->get();
-        // }
-        $client = Client::all();
-        $satpam = satpam::all();
+            $satpam = satpam::where('client_id' , $nitip->client_id)
+            ->get();
+            return view('/Satpam/DeploymentSatpam',['satpam' => $satpam] );
+        }
         
-        $profil = profilesatpam::with([`satpam`=>function($query)
-        {$query->where('satpam_id',$satpam->id);}])->get();
         
-
-        return view('/Satpam/DeploymentSatpam',['satpam' => $satpam, 'profil' => $profil, 'client' => $client] );
+        
         
     }
+
+    public function attendance($role){
+        $data = Crypt::decrypt($role);
+        $nitip=User::findOrFail($data);
+
+        if ($nitip->role =='admin') {
+           
+            $satpam = satpam::all();
+
+            return view('/Satpam/Attendance',['satpam' => $satpam] );
+           
+        }
+        elseif($nitip->role  == 'outsourcing'){
+            $satpam = satpam::where('outsourcing_id' , $nitip->company_id)
+            ->first();
+           
+            return view('/Satpam/Attendance',['satpam' => $satpam] );
+
+        }elseif($nitip->role == 'client'){
+            
+            $satpam = satpam::where('client_id' , $nitip->client_id)
+            ->get();
+            return view('/Satpam/Attendance',['satpam' => $satpam] );
+        }
+        
+        
+        
+        
+    }
+
+    public function addschedule(){
+        return view('/Satpam/AddSchedule' );
+    }
+
+    public function store(Request $request, $role){
+        $data = Crypt::decrypt($role);
+        $nitip=User::findOrFail($data);
+        $this->validate($request,[
+    		'tanggal' => 'required|date',
+            'bulan' => 'required|integer',
+            'shift' => 'required',
+            'time' => 'required',
+            'time1' => 'required'
+        ]);
+        
+
+       
+        schedule::create([
+            'date' => $request->tanggal,
+            'month' => $request->bulan,
+            'shift' => $request->shift,
+            'check_in' => $request->time,
+            'check_out' => $request->time1,
+            'id_company' => $nitip->company_id,
+            'id_client' => $nitip->company_id,
+            'id_satpam' => '0'
+        ]);
+
+        
+
+
+    	return redirect('/Satpam/Addschedule');
+    }
+
+
+    public function schedule( $role){
+        $data = Crypt::decrypt($role);
+        $nitip=User::findOrFail($data);
+        if ($nitip->role =='admin') {
+           
+            $schedule = schedule::all();
+
+            return view('/Satpam/schedule',['schedule' => $schedule] );
+           
+        }
+        elseif($nitip->role  == 'outsourcing'){
+            $schedule = schedule::where('id_company' , $nitip->company_id)
+            ->first();
+           
+            return view('/Satpam/schedule',['schedule' => $schedule] );
+
+        }elseif($nitip->role == 'client'){
+            
+            $schedule = schedule::where('id_client' , $nitip->client_id)
+            ->get();
+            return view('/Satpam/schedule',['schedule' => $schedule] );
+        }
+        
+
+        
+    }
+
+    public function editschedule($id){
+        $schedule = schedule::where('id_schedule' , $id)->first();
+        
+       return view('/Satpam/EditSchedule',['schedule' => $schedule]);   
+    }
+
+    public function update(Request $request,$id){
+        
+
+        $this->validate($request,[
+    		'tanggal' => 'required|date',
+            'bulan' => 'required|integer',
+            'shift' => 'required',
+            'time' => 'required',
+            'time1' => 'required'
+        ]);
+        
+        
+        DB::table('daily_schedule')
+        ->where('id_schedule', $id)
+        ->update([
+           'date' => $request->tanggal,
+            'month' => $request->bulan,
+            'shift' => $request->shift,
+            'check_in' => $request->time,
+            'check_out' => $request->time1]);
+        
+           
+        
+       return redirect('home');   
+    }
+    public function deleteschedule($id)
+    {
+         
+        DB::table('daily_schedule')
+        ->where('id_schedule', $id)
+        ->delete();
+            return back();
+    }
+
+
+
 }
